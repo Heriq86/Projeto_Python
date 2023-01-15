5835bd8e6352d4881fa68b446c7c19330fa842fc
Henrique Ribeiro <84481957+Heriq86@users.noreply.github.com>
2023-01-15 18:33:21 -0300

    Add files via upload

---
+++ CalculoAbono.py
@@ -0,0 +1,33 @@
+print("Atividade A3 N1")
+print("=" * 20)
+print('Digite os Salarios para realizar o calculo de abono salarial dos seus colaboradores')
+print("Para execultar o Calculo digite 0")
+salarios = [] 
+while True:
+    salario = float(input('Salário: '))
+    if salario == 0:
+        break
+    salarios.append(salario)
+
+print('Detalhamento de Gastos e Abono')
+print("=" * 20)
+
+print('Salário - Abono')
+abono = 0
+minimo = 0
+abonototal = 0
+for s in salarios:
+    abono = (s * 20) / 100
+    if abono < 100:
+       abono = 100
+       minimo += 1
+       abonototal += abono
+    else:
+       abonototal += abono
+   
+    print('R$ {:.2f} - R$ {:.2f}'.format(s, abono))
+
+print('Foram processados {} colaboradores'.format(salarios))
+print('Total gasto com abonos: R$ {:.2f}'.format(abonototal))
+print('Valor mínimo pago a {} colaboradores'.format(minimo))
+print('Maior valor de abono pago: R$ {:.2f} '.format(abono))
